a6211a020f6b06cca990e9ec10be95695a2dc67a
Thomas Thirion <thirionthomas68100@gmail.com>
2022-09-07 15:33:02 +0200

    ajout suppression user easyAdmin ok

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -6,7 +6,13 @@ use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactory;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormEvent;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -22,9 +28,46 @@ class UserCrudController extends AbstractCrudController
 
             TextField::new('name'),
             EmailField::new('email'),
-            TextField::new('password'),
+            TextField::new('password')
 
         ];
+    }
+
+    public function createEditFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface
+    {
+        $formBuilder = parent::createEditFormBuilder($entityDto, $formOptions, $context);
+
+        $this->addEncodePasswordEventListener($formBuilder);
+
+        return $formBuilder;
+    }
+
+    public function createNewFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface
+    {
+        $formBuilder = parent::createNewFormBuilder($entityDto, $formOptions, $context);
+
+        $this->addEncodePasswordEventListener($formBuilder);
+
+        return $formBuilder;
+    }
+
+/**
+* @required
+*/
+    public function setEncoder(UserPasswordHasherInterface $passwordEncoder): void
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    private function addEncodePasswordEventListener(FormBuilderInterface $formBuilder)
+    {
+        $formBuilder->addEventListener(FormEvents::SUBMIT, function (FormEvent $event) {
+            /** @var User $user */
+            $user = $event->getData();
+            if ($user->getPassword()) {
+                $user->setPassword($this->passwordEncoder->hashPassword($user, $user->getPassword()));
+            }
+        });
     }
 
 }
